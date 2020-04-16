bd28d637d400d547c90d2faf255ae31c1ac18048
garc897 <62458331+garc897@users.noreply.github.com>
2020-04-16 12:37:55 -0700

    Update hw30.cpp

--- hw30.cpp
+++ hw30.cpp
@@ -3,7 +3,9 @@
 /* 
  * File:    hw30.cpp
  * Author:  Christopher Garcia
- * Purpose: 
+ * Purpose: Read a person’s name in the following format: first name, middle 
+ *          name or middle initial, and last name then returns their last, 
+ *          first (middle initial.)
  * Date:    April 15, 2020
  */
 
@@ -37,32 +39,32 @@ int main(int argc, char** argv)
             {
                 ++name;
                 if (p1 == 0)
-                    p1=i;
+                    p1=i+1;
                 else if (p2 == 0)
                 {
                     if (full.at(i) == '.')
-                    p2 = i+1;
+                    p2 = i+2;
                     else
-                    p2 = i;
+                    p2 = i+1;
                 }
                 else if (p3 == 0)
-                    p3 = i;
+                    p3 = i+1;
             } 
         }
     }
     
     // Print name accordingly
     cout << "Abbreviation: ";
-    first = full.substr(0,p1);
+    first = full.substr(0,p1-1);
     if (name == 2)
     {
-        last = full.substr(p1+1,p2-p1);
+        last = full.substr(p1,p2-p1);
         cout << last << ", " << first;
     }
     else if (name == 3)
     {
-        middle = full.substr(p1+1,1);
-        last = full.substr(p2+1,p3-p2);
+        middle = full.substr(p1,1);
+        last = full.substr(p2,p3-p2);
         cout << last << ", " << first << " " << middle << ".";
     }    
     
